ce171e4d6d8c104e9369d0de06b0b13730cf07d0
NicolasRibe <oloconicao@gmail.com>
2025-10-02 21:12:47 -0300

    Projeto - Manutenção

--- sistema-manutencao/sistema-manutencao/src/main/java/br/com/aweb/sistema_manutencao/controller/OrdemServiceController.java
+++ sistema-manutencao/sistema-manutencao/src/main/java/br/com/aweb/sistema_manutencao/controller/OrdemServiceController.java
@@ -3,13 +3,19 @@ package br.com.aweb.sistema_manutencao.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.aweb.sistema_manutencao.model.OrdemServico;
 import br.com.aweb.sistema_manutencao.service.OrdemServicoService;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/ordemServico")
@@ -31,11 +37,51 @@ public class OrdemServiceController {
         return new ModelAndView ("home", Map.of("buscaId", busca));
     }
 
+
+    @GetMapping("/new")
+    public ModelAndView criarOrdem(){
+        return new ModelAndView("form", Map.of("ordem", new OrdemServico()));
+    }
+
+    @PostMapping("/new")
+    public String criarOrdem(@Valid OrdemServico ordemServico, BindingResult result){
+        if(result.hasErrors())
+            return "form";
+        ordemServicoService.save(ordemServico);
+        return "redirect:/ordemServico";
+    }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView editOrdem(@PathVariable Long id) {
+        return new ModelAndView("form", Map.of("ordem", ordemServicoService.buscarOrdemId(id)));
     }
 
 
+    @PostMapping("/edit/{id}")
+    public String edit(@Valid OrdemServico ordemServico, BindingResult result){
+        if(result.hasErrors())
+            return "form";
+        ordemServicoService.save(ordemServico);
+        return"redirect:/ordemServico";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable Long id){
+        ordemServicoService.delete(id);
+        return"redirect:/ordemServico";
+    }
+
+    @PostMapping("/finalizar/{id}")
+    public String finalizar(@PathVariable Long id){
+        ordemServicoService.finalizaOrdemServico(id);
+        return "redirect:/ordemServico";
+    }
+
+}
+
+
     
 
     
     
-}
+
--- sistema-manutencao/sistema-manutencao/src/main/java/br/com/aweb/sistema_manutencao/service/OrdemServicoService.java
+++ sistema-manutencao/sistema-manutencao/src/main/java/br/com/aweb/sistema_manutencao/service/OrdemServicoService.java
@@ -1,13 +1,23 @@
 package br.com.aweb.sistema_manutencao.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+
 import org.springframework.stereotype.Service;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+
+
 
 import br.com.aweb.sistema_manutencao.model.OrdemServico;
 import br.com.aweb.sistema_manutencao.repository.OrdemServicoRepository;
+
 
 // Toda a Logica de negocio esta dentro desta classe;
 @Service
@@ -26,11 +36,11 @@ public class OrdemServicoService {
         // criados;
         return ordemServicoRepository.findAll();
     }
-
-    //Criando um metodo de busca por ID
+    // Criando um metodo de busca por ID
     // Esse metodo busca com parametro inserido(id)
-    //Apos isso chamamos a classe Pai que faz a tratativa com parametro ordem de serviço 
-    //para usarmos o metodo isPresent e gerarmos uma exeção caso nao existir o id
+    // Apos isso chamamos a classe Pai que faz a tratativa com parametro ordem de
+    // serviço
+    // para usarmos o metodo isPresent e gerarmos uma exeção caso nao existir o id
     public OrdemServico buscarOrdemId(Long id) {
         Optional<OrdemServico> ordemServico = ordemServicoRepository.findById(id);
         if (ordemServico.isPresent()) {
@@ -39,8 +49,26 @@ public class OrdemServicoService {
         throw new RuntimeException("ID da Ordem não encontrada !!");
     }
 
-    public OrdemServico criarOrdem(OrdemServico ordemServico){
+    public OrdemServico save(OrdemServico ordemServico) {
         return ordemServicoRepository.save(ordemServico);
+    }
+
+    public void delete(@PathVariable Long id) {
+       if (!ordemServicoRepository.existsById(id))
+            throw new RuntimeException("Erro ao excluir Ordem!");
+        ordemServicoRepository.deleteById(id);
+    }
+    
+    public void finalizaOrdemServico(Long id){
+
+        var ordemServico = ordemServicoRepository.findById(id);
+
+        if(!ordemServico.isPresent())
+          throw new RuntimeException("Erro ao finalizar Ordem!");
+        
+        ordemServico.get().setFinalizacaoOrdem(LocalDateTime.now());
+        ordemServicoRepository.save(ordemServico.get());
+
     }
 
 }
